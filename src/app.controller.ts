1f3df1c19fde510625487edab2d3789ae28ede37
Marcel Brasileiro <brasileirodev@gmail.com>
2024-03-15 21:00:31 -0300

    Configurando conexão com banco de dados

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
+import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService
+  ) {}
 
   @Get('')
   getHello(): string {
-    return this.appService.getHello()
+    return this.appService.getHello();
+  }
+
+  @Get('list-users')
+  async store() {
+    return await this.prismaService.user.findMany();
+  }
+  @Delete('delete-user')
+  async delete() {
+    return await this.prismaService.user.delete({
+      where: {
+        id: '3c3dcd5f-8229-474e-8bad-2913c86dd425',
+      },
+    });
+  }
+
+  @Post('create-user')
+  async create() {
+    return await this.prismaService.user.create({
+      data: {
+        name: 'Marcel Brasileiro',
+        email: 'brasileiroleao2@gmail.com',
+        password: '12345678',
+      },
+    });
   }
 }
